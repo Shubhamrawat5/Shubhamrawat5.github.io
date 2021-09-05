da55346c7ff65f3fd940b601cee5bcb099dd1013
Shubhamrawat5 <sr856161@gmail.com>
2021-09-05 23:25:10 +0530

    add loader with lazy component

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+import "../asserts/css/loader.css";
+
+function Loader() {
+  return (
+    <div id="loader-container">
+      <span></span>
+      <span></span>
+      <span></span>
+      <h3>Loading...</h3>
+    </div>
+  );
+}
+
+export default Loader;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import React, { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
+
+//minimum given time it will take to load App component (for showing loading loader everytime)
+const App = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import("./App")), 2300);
+  });
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={<Loader />}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById("root")
 );
